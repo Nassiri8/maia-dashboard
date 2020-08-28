d53f8c96a195c86f8fdee5beb00422d25249035e
Noureddine Nassiri <nassir_n@etna-alternance.net>
2020-08-28 15:17:44 +0200

    Cors problem, Scrum daily je dois marreter

--- API-Maia/app.js
+++ API-Maia/app.js
@@ -2,23 +2,31 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const MongoClient = require("mongodb").MongoClient;
 const ObjectId = require("mongodb").ObjectID;
+var cors = require('cors')
 //const fetch = require('node-fetch');
 'use strict';
 
 const CONNECTION_URL = "mongodb+srv://nour:rkBIE2JqLGX97Gzg@cluster0.xofne.mongodb.net/Cluster0?retryWrites=true&w=majority";
-const DATABASE_NAME = "test";
+const DATABASE_NAME = "Cluster0";
 //cryptage pwd
 const crypto = require('crypto');
 
 // create express app
 var app = express();
 
+//enable cors
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("--allow-file-access-from-files");
+    next();
+  });
+  
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }))
 
 // parse application/json
 app.use(bodyParser.json())
-
 
 const mongoose = require('mongoose');
 
@@ -33,17 +41,6 @@ mongoose.connection.on('error', function() {
 mongoose.connection.once('open', function() {
     console.log("Successfully connected to the database");
 })
-//code pour crypter en sha512
-
-var sha512 = function(password, salt){
-    var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
-    hash.update(password);
-    var value = hash.digest('hex');
-    return {
-	salt:salt,
-	passwordHash:value
-    };
-};
 
 //routes:
 require('./src/routes/user.routes.js')(app);
